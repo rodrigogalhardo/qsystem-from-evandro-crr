18a1964953f73c9ba3fac35f78f375731df5d085
Evandro Chagas <ev.crr97@gmail.com>
2019-04-03 21:07:09 -0300

    in header valid

--- header/qsystem.h
+++ header/qsystem.h
@@ -126,18 +126,6 @@ class QSystem {
     cut_pair        cut(size_t &target, vec_size_t &control);
     void            fill(Gate_aux::Tag tag, size_t qbit, size_t size_n);
 
-    /* src/qs_valid.cpp */
-    void            valid_qbit(std::string name, size_t qbit);
-    void            valid_count(size_t qbit, size_t count, size_t size_n=1);
-    void            valid_control(vec_size_t &control);
-    void            valid_phase(complex phase);
-    void            valid_swap(size_t qbit_a, size_t qbit_b);
-    void            valid_range(size_t qbegin, size_t qend);
-    void            valid_gate(char gate);
-    void            valid_p(double p);
-    void            valid_state();
-    void            valid_krau(vec_str &kraus);
-
     /*--------------------*/
     Gates&           gates;
     size_t          _size;
@@ -150,5 +138,124 @@ class QSystem {
     size_t          an_size;
     Gate_aux*       an_ops;
     Bit*            an_bits;
+
+    /* src/qs_valid.cpp */
+    inline void     valid_qbit(std::string name, size_t qbit);
+    inline void     valid_count(size_t qbit, size_t count, size_t size_n=1);
+    inline void     valid_control(vec_size_t &control);
+    inline void     valid_phase(complex phase);
+    inline void     valid_swap(size_t qbit_a, size_t qbit_b);
+    inline void     valid_range(size_t qbegin, size_t qend);
+    inline void     valid_gate(char gate);
+    inline void     valid_p(double p);
+    inline void     valid_state();
+    inline void     valid_krau(vec_str &kraus);
+
 };
+
+/******************************************************/
+inline void QSystem::valid_qbit(std::string name, size_t qbit) {
+  if (qbit >= size()) {
+      sstr err;
+      err << "\'" << name << "\' argument should be in the range of 0 to "
+          << (size()-1);
+      throw std::invalid_argument{err.str()};
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_count(size_t qbit, size_t count, size_t size_n) {
+  if (count == 0 and qbit+count*size_n <= size()) {
+      sstr err;
+      err << "\'cout\' argument should be greater than 0 "
+          << "and \'qbit+count\' suld be in the range of 0 to "
+          << size();
+      throw std::invalid_argument{err.str()};
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_control(vec_size_t &control) {
+  if (control.size() == 0) {
+    sstr err;
+    err << "\'control\' argument must have at least one item";
+    throw std::invalid_argument{err.str()};
+  }
+  for (auto& i : control) {
+    if (i >= size()) {
+      sstr err;
+      err << "Items in \'control\' should be in the range of 0 to "
+          << (size()-1);
+      throw std::invalid_argument{err.str()};
+    }
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_phase(complex phase) {
+  if (std::abs(std::abs(phase) - 1.0) > 1e-14) {
+    sstr err;
+    err << "abs(phase) must be equal to 1";
+    throw std::invalid_argument{err.str()};
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_swap(size_t qbit_a, size_t qbit_b) {
+  if (qbit_a >= size() or qbit_b >= size()) {
+      sstr err;
+      err << "Arguments \'qbit_a\' and \'qbit_b\' should be in the "
+          << "range of 0 to " << (size()-1);
+      throw std::invalid_argument{err.str()};
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_range(size_t qbegin, size_t qend) {
+  if (qbegin >= size() or qend > size() or qbegin <= qend) {
+      sstr err;
+      err << "\'qbegin\' argument should be in the "
+          << "range of 0 to " << (size()-1)
+          << " and argument \'qend\' should be greater than 0 "
+          << "and in the range of 0 to " << size();
+      throw std::invalid_argument{err.str()};
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_gate(char gate) {
+  if (not (gate == 'X' or gate == 'Y' or gate == 'Z')) {
+    sstr err;
+    err << "\'gate\' argument must be equal to \'X\', \'Y\' or \'X\'";
+    throw std::invalid_argument{err.str()};    
+  }
+}
+
+/******************************************************/
+inline void QSystem::valid_p(double p) {
+  if (p < 0 or p > 1) {
+    sstr err;
+    err << "\'p\' argument should be in the range of 0.0 to 1.0";
+    throw std::invalid_argument{err.str()};
+  }
+}
+
+inline void QSystem::valid_state() {
+  if (_state == "vector") {
+    sstr err;
+    err << "\'state\' must be in \"matrix\" to apply this channel";
+    throw std::runtime_error{err.str()};
+  }
+}
+
+inline void QSystem::valid_krau(vec_str &kraus) {
+  size_t ksize = kraus[0].size();
+  for (auto& k : kraus) {
+    if (k.size() != ksize) {
+      sstr err;
+      err << "All \'kraus\' operators must have the same size";
+      throw std::runtime_error{err.str()};
+    }
+  }
+}
 
